c267ca3d96d7b064cc427486cbf46a1cfcc33e81
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-08-29 10:37:22 -0300

    Adicionados getters e setters

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -7,7 +7,7 @@ public class ClasseAutomotores {
 	String tipoAt;
 	
 	//Construtor vazio
-	public ClasseAnimais() {
+	public void ClasseAnimais() {
 		
 	}
 	
@@ -17,6 +17,35 @@ public class ClasseAutomotores {
 		this.corAt = corPar;
 		this.marcaAt = marcaPar;
 		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
+	}
+	
+	//Getters (public + tipo + return)
+	public String pegarCor() {
+		return corAt;
+	}
+	public String pegarMarca() {
+		return marcaAt;
+	}
+	public String pegarModelo() {
+		return modeloAt;
+	}
+	public String pegaTipo() {
+		return tipoAt;
+	}
+	
+	//Setters (public void + parâmetros + this)
+	
+	public void AlterarCor(String corPar){
+		this.corAt = corPar;
+	}
+	public void AlterarMarca(String marcaPar) {
+		this.marcaAt = marcaPar;
+	}
+	public void AlterarModelo(String modeloPar) {
+		this.modeloAt = modeloPar;
+	}
+	public void AlterarTipo(String tipoPar) {
 		this.tipoAt = tipoPar;
 	}
 }
