ae48e22961c75bade752ae34763cdb2dad266d2e
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-08-29 10:16:28 -0300

    Adicionados construtores na ClasseAutomotores

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,4 +1,22 @@
 
 public class ClasseAutomotores {
-
+	
+	String corAt;
+	String marcaAt;
+	String modeloAt;
+	String tipoAt;
+	
+	//Construtor vazio
+	public ClasseAnimais() {
+		
+	}
+	
+	//Construtor
+	public ClasseAutomotores(String corPar, String marcaPar, String modeloPar, String tipoPar) {
+		
+		this.corAt = corPar;
+		this.marcaAt = marcaPar;
+		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
+	}
 }
